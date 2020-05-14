e5d2b4b2284a304f439540f588d2fc7f8e705b89
mavrin88 <mavrin_88@mail.ru>
2020-05-14 14:49:26 +0300

    v3.13

--- database/migrations/2020_05_14_054420_create_contract_pays_table.php
+++ database/migrations/2020_05_14_054420_create_contract_pays_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCotractPaysTable extends Migration
+class CreateContractPaysTable extends Migration
 {
     /**
      * Run the migrations.
